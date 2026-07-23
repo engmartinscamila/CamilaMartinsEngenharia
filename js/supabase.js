5568b8ee2ec86fa06b7c8b74f990c1b484896d25
engmartinscamila <eng.martins.camila@gmail.com>
2026-07-23 18:54:39 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,26 @@
-// CONEXÃO SUPABASE
+/*
+====================================
+CAMILA MARTINS ENGENHARIA
+CONEXÃO SUPABASE
+====================================
+*/
+
 
 const SUPABASE_URL = "https://hghtwlopqztfcosfxafd.supabase.co";
 
+
 const SUPABASE_KEY = "sb_publishable_-unXLR2NSSACLs01Sr60GA_uCFnJ74f";
 
-const supabaseClient = supabase.createClient(
+
+// Criar conexão global
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+
+// Atalho global para os scripts antigos
+window.supabaseDB = window.supabaseClient;
+
+
+console.log("SUPABASE CONECTADO COM SUCESSO");
