dcfefd608ad69ebfdd94af09bb12486d8d9faf40
engmartinscamila <eng.martins.camila@gmail.com>
2026-08-30 21:58:52 -0300

    config: ativa Firebase Web Push no portal

--- js/firebase-push-config.js
+++ js/firebase-push-config.js
@@ -5,13 +5,13 @@
     // Estes valores NÃO são segredos. O envio servidor->FCM usa uma credencial
     // privada separada no Supabase e nunca deve ser colocado neste arquivo.
     scope.CME_FIREBASE_PUSH_CONFIG = Object.freeze({
-        enabled: false,
-        apiKey: "",
-        authDomain: "",
+        enabled: true,
+        apiKey: "AIzaSyAOrZvZQy3RmtZSgIGsaV01lJD30CrIZYI",
+        authDomain: "camilamartinsengenharia-88a9d.firebaseapp.com",
         projectId: "camilamartinsengenharia-88a9d",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: "",
+        storageBucket: "camilamartinsengenharia-88a9d.firebasestorage.app",
+        messagingSenderId: "474097139718",
+        appId: "1:474097139718:web:adf772e34ce58ca3220387",
         vapidKey: "BIYTZ0zLyZ8CFw7O5vXTWrExBIFTyJx0nmthHWc0RV_Mw3clWpRid9FrYn1QWur8D_TWjOwV8Yrxh04mFQKuKfM"
     });
 }(typeof self !== "undefined" ? self : window));
