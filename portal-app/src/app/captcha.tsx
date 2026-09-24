864111776a4374fbc81f9458e8984b906e09c2f5
engmartinscamila <eng.martins.camila@gmail.com>
2026-09-23 22:32:55 -0300

    Usar deep link seguro após validação CAPTCHA

--- portal-app/src/app/captcha.tsx
+++ portal-app/src/app/captcha.tsx
@@ -1,6 +1,6 @@
 import { useLocalSearchParams } from 'expo-router';
 import React, { useCallback, useEffect, useState } from 'react';
-import { Platform } from 'react-native';
+import * as Linking from 'expo-linking';\nimport { Platform } from 'react-native';
 
 import { AuthShell } from '@/components/auth-shell';
 import { TurnstileCaptcha } from '@/components/turnstile-captcha';
