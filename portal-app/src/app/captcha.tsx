5be862889f3dd1c107fbe6ce7b0980b5f6600be2
engmartinscamila <eng.martins.camila@gmail.com>
2026-09-23 22:34:06 -0300

    Corrigir imports do fluxo CAPTCHA

--- portal-app/src/app/captcha.tsx
+++ portal-app/src/app/captcha.tsx
@@ -1,6 +1,7 @@
 import { useLocalSearchParams } from 'expo-router';
 import React, { useCallback, useEffect, useState } from 'react';
-import * as Linking from 'expo-linking';\nimport { Platform } from 'react-native';
+import * as Linking from 'expo-linking';
+import { Platform } from 'react-native';
 
 import { AuthShell } from '@/components/auth-shell';
 import { TurnstileCaptcha } from '@/components/turnstile-captcha';
